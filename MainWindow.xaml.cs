ecc87fde736714b3e993caa4861ee072d55b8c57
lsylusiyao <lsylusiyao@qq.com>
2024-05-09 23:34:31 +1200

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FlatmateLivingBillShare
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = float.TryParse(e.Text, out _);
+        }
+
+        private void DeleteMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            // YourDataModel selectedItem = dataGrid.SelectedItem;
+            // YourData.Remove(selectedItem);
+        }
+    }
+}
